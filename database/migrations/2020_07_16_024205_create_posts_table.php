eebc89ea544f245b0da07d9e77216493c1fc5b9a
sasaya7016 <sasaya7016@outlook.jp>
2020-07-16 08:13:03 +0900

    Initial commit

---
+++ database/migrations/2020_07_16_024205_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('firstname', 20);
+            $table->string('lastname', 20); 
+            $table->string('text')->nullable();
+            $table->string('email', 100)->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
