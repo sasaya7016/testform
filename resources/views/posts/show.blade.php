42b559ece1da464a01f45639af9a2ba8adaa5e45
sasaya7016 <sasaya7016@outlook.jp>
2020-07-18 13:49:34 +0900

    Update show.blade.php

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,56 +1,37 @@
-<!DOCTYPE html>
-<html lang="ja">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'testform') }}</title>
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-  </head>
+@extends('app')
 
-  <body>
-  <div class="container">
-    <h4 class="text-center font-weight-bold">フォーム</h4>
+@section('content')
+@include('nav')
 
-    @if ($errors->any())
-      <div class="alert alert-danger">
-        <ul>
-          @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-          @endforeach
-        </ul>
+<div class="container">
+  <h4 class="text-center font-weight-bold">フォーム</h4>
+
+  @if ($errors->any())
+    <div class="alert alert-danger">
+      <ul>
+        @foreach ($errors->all() as $error)
+          <li>{{ $error }}</li>
+        @endforeach
+      </ul>
+    </div>
+  @endif
+
+  <form method="GET" action="">
+    @csrf
+    <div class="card">
+      <div class="card-header text-center">
+        詳細
       </div>
-    @endif
+      <ul class="list-group ">
+        <li class="list-group-item">ID : {{$post->id}}</li>
+        <li class="list-group-item">苗字 : {{$post->lastname}}</li>
+        <li class="list-group-item">氏名 : {{$post->firstname}}</li>
+        <li class="list-group-item">メールアドレス : {{$post->email}}</li>
+        <li class="list-group-item">コメント : {{$post->text}}</li>
+      </ul>
+    </div>
+  </form>
+  <a href="{{ url('posts/index') }}">戻る</a>
 
-    <form method="GET" action="">
-      @csrf
-      <div class="card">
-        <div class="card-header text-center">
-          詳細
-        </div>
-        <ul class="list-group ">
-          <li class="list-group-item">ID : {{$post->id}}</li>
-          <li class="list-group-item">苗字 : {{$post->lastname}}</li>
-          <li class="list-group-item">氏名 : {{$post->firstname}}</li>
-          <li class="list-group-item">メールアドレス : {{$post->email}}</li>
-          <li class="list-group-item">コメント : {{$post->text}}</li>
-        </ul>
-      </div>
-    </form>
-    <a href="{{ url('posts/index') }}">戻る</a>
-
-    
-  </div>
-
-  <!-- Optional JavaScript -->
-  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-
-
-  </body>
-</html>
+  
+</div>
