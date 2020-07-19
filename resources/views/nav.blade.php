d28187955cabd83be226cf5c19c54bd06418c9ad
sasaya7016 <sasaya7016@outlook.jp>
2020-07-19 14:45:21 +0900

    Update nav.blade.php

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,17 +1,3 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#">Test Form</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item">
-        <a class="nav-link disabled" href="/">Import</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="/">Export</a>
-      </li>
-    </ul>
-  </div>
 </nav>
